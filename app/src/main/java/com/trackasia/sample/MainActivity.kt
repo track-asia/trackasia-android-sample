00c97556b806c2601f2135915706b04bd01c4a63
Sang Nguyen <vsang25@gmail.com>
2023-07-29 20:14:58 +0700

    #Change - Update Trackasia Android Sample

---
+++ app/src/main/java/com/trackasia/sample/MainActivity.kt
@@ -0,0 +1,80 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+package com.trackasia.sample
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.trackasia.android.Trackasia
+import com.trackasia.android.maps.MapView
+import com.trackasia.android.maps.Style
+import com.trackasia.android.camera.CameraPosition
+import com.trackasia.android.geometry.LatLng
+
+private const val SERVICE_NAME = "geo"
+
+class MainActivity : AppCompatActivity() {
+    private var mapView: MapView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // initialize MapLibre
+        Trackasia.getInstance(this)
+
+        // initialize the view
+        setContentView(R.layout.activity_main)
+
+        // initialize the map view
+        mapView = findViewById(R.id.mapView)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync { map ->
+            map.setStyle(
+                Style.Builder()
+                    .fromUri("https://demotiles.track-asia.com/style.json")
+            ) { style ->
+                findViewById<TextView>(R.id.attributionView).text = style.sources.first()?.attribution
+            }
+            map.cameraPosition = CameraPosition.Builder()
+                .target(LatLng(0.0, -75.0))
+                .zoom(1.0)
+                .build()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
+}
